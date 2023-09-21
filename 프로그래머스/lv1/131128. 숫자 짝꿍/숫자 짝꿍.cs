7d08040c270f88d61b8d3aee1df5bbd2f5f3fd14
subscriptioncat <119589973+subscriptioncat@users.noreply.github.com>
2023-09-21 10:37:59 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.35 ms, Memory: 30.5 MB -BaekjoonHub

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.cs
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.cs
@@ -5,30 +5,41 @@ using System.Linq;
 
 public class Solution {
     public string solution(string X, string Y) {
-        StringBuilder sb = new StringBuilder();
-
-        int[] intsX = new int[10];
-        int[] intsY = new int[10];
-        int[] intsXY = new int[10];
-
-        foreach (Char c in X)
-            intsX[c - '0']++;
-        foreach (Char c in Y)
-            intsY[c - '0']++;
-        for(int i =0; i < 10; i++)
-            intsXY[i] = Math.Min(intsX[i], intsY[i]);
-        for (int i = 9; i >= 0; i--)
+        List<char> charList = new List<char>();
+        if(X.Length >= Y.Length)
         {
-            for (int j = 0; j < intsXY[i]; j++)
-                sb.Append(i);
+            foreach (char c in X)
+            {
+                if (Y.Length == 0)
+                    break;
+                int YIndex = Y.IndexOf(c);
+                if (YIndex >= 0)
+                {
+                    charList.Add(c);
+                    Y = Y.Remove(YIndex, 1);
+                }
+            }
         }
-
-        if (sb.Length == 0)
+        else
+        {
+            foreach (char c in Y)
+            {
+                if (X.Length == 0)
+                    break;
+                int XIndex = X.IndexOf(c);
+                if (XIndex >= 0)
+                {
+                    charList.Add(c);
+                    X = X.Remove(XIndex, 1);
+                }
+            }
+        }
+            
+        if(charList.Count == 0)
             return "-1";
+        charList.Sort((n1, n2) => n2.CompareTo(n1));
+        string answer = int.Parse(new string(charList.ToArray())).ToString();
 
-        if (sb[0] == '0')
-            return "0";
-
-        return sb.ToString();
+        return answer;
     }
 }

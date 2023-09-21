323b11ac5fb709d56cd527a1aacebde570a0f0bd
subscriptioncat <119589973+subscriptioncat@users.noreply.github.com>
2023-09-21 10:37:18 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.35 ms, Memory: 30.5 MB -BaekjoonHub

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.cs
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.cs
@@ -6,26 +6,27 @@ using System.Linq;
 public class Solution {
     public string solution(string X, string Y) {
         StringBuilder sb = new StringBuilder();
-        int[] ints1 = new int[10];
-        int[] ints2 = new int[10];
+
+        int[] intsX = new int[10];
+        int[] intsY = new int[10];
+        int[] intsXY = new int[10];
 
         foreach (Char c in X)
-            ints1[c - '0']++;
+            intsX[c - '0']++;
         foreach (Char c in Y)
-            ints2[c - '0']++;
-
+            intsY[c - '0']++;
+        for(int i =0; i < 10; i++)
+            intsXY[i] = Math.Min(intsX[i], intsY[i]);
         for (int i = 9; i >= 0; i--)
         {
-            int count = ints1[i] < ints2[i] ? ints1[i] : ints2[i];
-
-            for (int j = 0; j < count; j++)
+            for (int j = 0; j < intsXY[i]; j++)
                 sb.Append(i);
         }
 
         if (sb.Length == 0)
             return "-1";
 
-        if (sb.ToString().ToCharArray().Where<Char>(x => x == '0').Count() == sb.Length)
+        if (sb[0] == '0')
             return "0";
 
         return sb.ToString();

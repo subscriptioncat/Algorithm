151a30a5982c8e096429fc58b6fb98137b2f32b5
subscriptioncat <119589973+subscriptioncat@users.noreply.github.com>
2024-02-13 18:48:43 +0900

    [level 2] Title: 타겟 넘버, Time: 1.89 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 31.4 MB, 시간: 0.76 ms
+메모리: 31.4 MB, 시간: 1.89 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 2일 18:39:43
+2024년 2월 2일 18:48:42
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cs
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cs
@@ -1,34 +1,39 @@
- using System;
+using System;
+using System.Linq;
+public class Solution 
+{
+    int answer;
+    bool[] visitArray;
+    int[] numberArray;
+    int numberArrayLength;
 
-    public class Solution
+    public int solution(int[] numbers, int target) 
     {
-        public int count = 0;
+        int sum = numbers.Sum();
+        int k = (sum - target) / 2;
 
-        public int solution(int[] numbers, int target)
+        answer = 0;
+        numberArray = numbers.Where(x => x <= k).ToArray();
+        numberArrayLength = numberArray.Length;
+        visitArray = new bool[numberArrayLength];
+
+        Dfs(k, 0);
+        return answer = answer == 0 ? 1 : answer;
+    }
+
+    public void Dfs(int target, int count)
+    {
+        for(int i = count; i < numberArrayLength; i++)
         {
-            Function(numbers, target, -1);
-
-            return count;
-        }
-
-        public void Function(int[] numbers, int target, int index)
-        {
-            index++;
-            
-            if (numbers.Length == index)
-            {
-                int total = 0;
-                foreach (int num in numbers)
-                    total += num;
-
-                if (total == target)
-                    count++;
-
-                return;
+            if (!visitArray[i] && target >= numberArray[i])
+            {              
+                visitArray[i] = true;
+                if(target != numberArray[i])
+                    Dfs(target - numberArray[i], i);
+                else
+                    answer++;
+                visitArray[i] = false;
             }
-            
-            Function(numbers, target, index);
-            numbers[index] *= -1;
-            Function(numbers, target, index);
         }
     }
+}

1b96a071106c54812a2b9e892d872d92463c5cff
subscriptioncat <119589973+subscriptioncat@users.noreply.github.com>
2024-02-13 18:39:45 +0900

    [level 2] Title: 타겟 넘버, Time: 0.76 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cs
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cs
@@ -1,38 +1,34 @@
-using System;
-using System.Linq;
-public class Solution 
-{
-    int answer;
-    bool[] visitArray;
-    int[] numberArray;
-    int numberArrayLength;
-    
-    public int solution(int[] numbers, int target) 
+ using System;
+
+    public class Solution
     {
-        int sum = numbers.Sum();
-        int k = (sum - target) / 2;
-        
-        answer = 0;
-        numberArray = numbers.Where(x => x <= k).ToArray();
-        numberArrayLength = numberArray.Length;
-        visitArray = new bool[numberArrayLength];
-        
-        Dfs(k, 0);
-        return answer = answer == 0 ? 1 : answer;
-    }
-    
-    public void Dfs(int target, int count)
-    {
-        for(int i = count; i < numberArrayLength; i++)
+        public int count = 0;
+
+        public int solution(int[] numbers, int target)
         {
-            if (!visitArray[i] && target >= numberArray[i])
+            Function(numbers, target, -1);
+
+            return count;
+        }
+
+        public void Function(int[] numbers, int target, int index)
+        {
+            index++;
+            
+            if (numbers.Length == index)
             {
-                visitArray[i] = true;
-                Dfs(target - numberArray[i], i);
-                visitArray[i] = false;
+                int total = 0;
+                foreach (int num in numbers)
+                    total += num;
+
+                if (total == target)
+                    count++;
+
+                return;
             }
-        } 
-        if(target == 0)
-            answer++;
+            
+            Function(numbers, target, index);
+            numbers[index] *= -1;
+            Function(numbers, target, index);
+        }
     }
-}

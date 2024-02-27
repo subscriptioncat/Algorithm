144e565c155dd0f41ff823d2683f91350724bc2e
subscriptioncat <119589973+subscriptioncat@users.noreply.github.com>
2024-02-27 12:50:29 +0900

    [level 2] Title: 소수 찾기, Time: 6.22 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Solution {
+    bool[] visitArr;
+    int[] numberArr;
+    int numberArrLength;
+    HashSet<int> hs;
+    
+    public int solution(string numbers) 
+    {
+        numberArr = numbers.Select(item => (int)char.GetNumericValue(item)).ToArray();
+        numberArrLength = numberArr.Length;
+        visitArr = new bool[numberArrLength];
+        hs = new HashSet<int>();
+        DFSPlay(0);
+        Filtering();
+        return GetHashSetCount();
+    }
+    
+    public void DFSPlay(int num)
+    {
+        for (int i = 0; i < numberArrLength; i++)
+        {
+            if (!visitArr[i])
+            {
+                num *= 10;
+                num += numberArr[i];
+                visitArr[i] = true;
+
+                hs.Add(num);
+                DFSPlay(num);
+
+                visitArr[i] = false;
+                num /= 10;
+            }
+        }
+    }
+
+    public int GetHashSetCount()
+    {
+        return hs.Count;
+    }
+
+    public void PrintHashSet()
+    {
+        foreach (int i in hs)
+            Console.WriteLine(i);
+    }
+
+    public void Filtering()
+    {
+        int max = hs.Max();
+        bool[] isPrime = new bool[max+1];
+        hs.Remove(0);
+        hs.Remove(1);
+        for (int i = 2; i * i <= max; i++)
+        {
+            if (!isPrime[i])
+            {
+                for (int j = i * i; j <= max; j += i)
+                {
+                    isPrime[j] = true;
+                    hs.Remove(j);
+                }
+            }
+        }
+    }
+}

7e3befcd156c0a8e5dfe073cd83411fbd48e57c4
subscriptioncat <119589973+subscriptioncat@users.noreply.github.com>
2024-09-11 13:43:48 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 43.10 ms, Memory: 42.3 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.cs
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.cs
@@ -3,69 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 
 public class Solution {
-    int[] _diffs;
-    int[] _times;
-    long _limit;
-    int _diffsCount;
+    public int solution(int[] diffs, int[] times, long limit) {
+        int level = 0;
+        int diffsCount = diffs.Count();
+        long time = long.MaxValue;
+        int time_prev = 0;
 
-    public int solution(int[] diffs, int[] times, long limit)
-    {
-        _diffs = diffs;
-        _times = times;
-        _limit = limit;
-        _diffsCount = diffs.Count();
-
-        int low = 1;
-        int high = diffs.Max();
-
-        return BinarySearch(low, high);
-    }
-
-    public int BinarySearch(int low, int high)
-    {
-        if (low > high)
-            return low;
-
-        int mid = (low + high) / 2;
-
-        if (CheckingLevelUpAndDown(mid))
+        while(limit < time)
         {
-            return BinarySearch(low, mid - 1);
-        }
-        else
-        {
-            return BinarySearch(mid + 1, high);
-        } 
-    }
-
-    public bool CheckingLevelUpAndDown(int level)
-    {
-        long time_curr;
-        int time_prev;
-
-        //한 Level를 검사하는 문장
-        time_curr = 0;
-        time_prev = 0;
-        for (int i = 0; i < _diffsCount; i++)
-        {
-            if (_diffs[i] > level)
+            level++;
+            time = 0;
+            time_prev = 0;
+            for (int i = 0; i < diffsCount; i++)
             {
-                time_curr += (time_prev + _times[i]) * (_diffs[i] - level) + _times[i];
+                if (diffs[i] > level)
+                {
+                    time += (time_prev + times[i]) * (diffs[i] - level) + times[i];
+                }
+                else
+                {
+                    time += times[i];
+                }
+                time_prev = times[i];
             }
-            else
-            {
-                time_curr += _times[i];
-            }
-            time_prev = _times[i];
         }
-
-        if(time_curr > _limit)
-        {
-            return false;
-        }
-        else
-        {
-            return true;
-        }
+        return level;
     }
 }

e9dbf166299adbb8798a48cf6fe97e73fbbee186
subscriptioncat <119589973+subscriptioncat@users.noreply.github.com>
2024-10-11 08:11:29 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.59 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Linq;
+class Solution 
+{
+    public int solution(int n) 
+   {
+        int oneCount = Convert.ToString(n,2).Count(x => x == '1');
+        while (true)
+        {
+            if (Convert.ToString(++n, 2).ToString().Count(x => x == '1') == oneCount)
+                break;
+        }
+
+        return n;
+    }
+}
